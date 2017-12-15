cc763cdb8f93d7ee071ebdf5ab88aa923d05e002
Luke Kali <lovesanxx@users.noreply.github.com>
2017-12-15 13:44:31 -0600

    Generate template files

--- src/project.rs
+++ src/project.rs
@@ -18,7 +18,24 @@ impl Project {
     /// Creates a new project and returns it's properties.
     pub fn new(name: &str, target: String) -> Project {
         // Create the project directory
-        DirBuilder::new().create(format!("./{}", name)).unwrap();
+        let mut dir_builder = DirBuilder::new();
+        dir_builder.create(format!("./{}", name)).unwrap();
+        dir_builder.create(format!("./{}/source", name)).unwrap();
+        dir_builder.create(format!("./{}/include", name)).unwrap();
+
+        // Create the template main.c source file
+        let mut source_file = File::create(format!("./{}/source/main.c", name)).unwrap();
+        write!(source_file, "{}",
+r#"#include <stdout.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello, world!\n");
+    return 0;
+}
+"#).unwrap();
+        source_file.sync_data().unwrap();
+
         // Create the project file in the new folder
         let mut project_file = File::create(format!("./{}/maid.toml", name)).unwrap();
 
@@ -31,6 +48,7 @@ impl Project {
                 target: target,
             }
         };
+
         // Serialize the project into TOML
         let toml: String = ::toml::to_string(&project).unwrap();
 
@@ -43,13 +61,16 @@ impl Project {
     }
     /// Gets the Project in the directory given (no "/" at the end)
     pub fn get(dir: &str) -> Project {
-        assert!(!dir.ends_with("/")); // Ensure the given directory doesn't end with a "/"
+        // Ensure the given directory doesn't end with a "/"
+        assert!(!dir.ends_with("/"));
+
         // Open the project file
         let mut project_file = File::open(format!("{}/maid.toml", dir)).unwrap();
 
         let mut contents = String::new();
         // Read the file into the String `contents`
         project_file.read_to_string(&mut contents).unwrap();
+
         // Deserialize the TOML
         let project: Project = ::toml::from_str(contents.as_str()).unwrap();
         println!("{:?}", project);
